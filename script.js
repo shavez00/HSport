b9ddc9c49fc00598092d7dfef1cad7018021de52
shavez00 <shavez00@Yahoo.com>
2018-04-09 18:11:44 -0700

    estimate total now greyed out until shipping state selected

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 (function() {
 "use strict";
+
+var state = document.getElementById("s-state");
+
+document.addEventListener("DOMContentLoaded", function() {
+	document.getElementById("cart-hplus").addEventListener("submit", estimateTotal);
 	
-document.getElementById("cart-hplus").addEventListener("submit", estimateTotal);
+	var btnEstimate = document.getElementById("btn-estimate");
+	
+	btnEstimate.disabled = true;
+	
+	state.addEventListener("change", function(){
+		if (state.value === "") {
+			btnEstimate.disabled = true;
+  	} else {
+	  	btnEstimate.disabled = false;
+ 	 }
+	});
+});
 
 function estimateTotal(event) {
   event.preventDefault();
-
-  var state = document.getElementById("s-state");
 
   if (state.value === "") {
 		window.alert("Please enter a state");
